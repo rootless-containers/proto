rootless-containers/proto

commit 7501d1e97fe34b410f8f34843dd2e80eacfeaa1a

go-proto: regenerate rootlesscontainers.pb.go
Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

go-proto/rootlesscontainers.pb.go
@@ -15,14 +15,14 @@
 
 // Code generated by protoc-gen-go. DO NOT EDIT.
 // versions:
-// 	protoc-gen-go v1.27.1
-// 	protoc        v3.12.4
+// 	protoc-gen-go v1.28.1
+// 	protoc        v3.21.8
 // source: rootlesscontainers.proto
 
 // The rootlesscontainers package is maintained at https://github.com/rootless-containers/proto .
 // If you want to extend the resource definition, please open a PR.
 
-package rootlesscontainers
+package go_proto
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -109,8 +109,11 @@ var file_rootlesscontainers_proto_rawDesc = []byte{
 	0x6c, 0x65, 0x73, 0x73, 0x63, 0x6f, 0x6e, 0x74, 0x61, 0x69, 0x6e, 0x65, 0x72, 0x73, 0x22, 0x2e,
 	0x0a, 0x08, 0x52, 0x65, 0x73, 0x6f, 0x75, 0x72, 0x63, 0x65, 0x12, 0x10, 0x0a, 0x03, 0x75, 0x69,
 	0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x03, 0x75, 0x69, 0x64, 0x12, 0x10, 0x0a, 0x03,
-	0x67, 0x69, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x03, 0x67, 0x69, 0x64, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x67, 0x69, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x03, 0x67, 0x69, 0x64, 0x42, 0x2f,
+	0x5a, 0x2d, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x72, 0x6f, 0x6f,
+	0x74, 0x6c, 0x65, 0x73, 0x73, 0x2d, 0x63, 0x6f, 0x6e, 0x74, 0x61, 0x69, 0x6e, 0x65, 0x72, 0x73,
+	0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x6f, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (

commit 4cd87ebadd670f849a518618bc9862e703f9baa5

go-proto: fix import errors
The previous update to rootlesscontainers.pb.go caused the package name
of the generated Go file to change, causing the following import errors
when trying to import the go-proto into any other project:

> found packages go_proto (rootlesscontainers.pb.go)
> and rootlesscontainers (rootlesscontainers_generate.go) in
> .../github.com/rootless-containers/proto/go-proto

Running "go generate ./rootlesscontainers_generate.go" solves this
issue.

Fixes: commit 7501d1e97fe3 ("go-proto: regenerate rootlesscontainers.pb.go")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

go-proto/rootlesscontainers.pb.go
@@ -15,14 +15,14 @@
 
 // Code generated by protoc-gen-go. DO NOT EDIT.
 // versions:
-// 	protoc-gen-go v1.28.1
-// 	protoc        v3.21.8
+// 	protoc-gen-go v1.30.0
+// 	protoc        v3.21.12
 // source: rootlesscontainers.proto
 
 // The rootlesscontainers package is maintained at https://github.com/rootless-containers/proto .
 // If you want to extend the resource definition, please open a PR.
 
-package go_proto
+package rootlesscontainers
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"